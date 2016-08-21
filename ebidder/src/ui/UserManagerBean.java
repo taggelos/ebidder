6cb01cdc1296bc3ee4837de2975dfdb27a7ab1ad
taggelos <aggelos5991@gmail.com>
2016-08-22 00:27:51 +0300

    prev next koumpia

--- ebidder/src/ui/UserManagerBean.java
+++ ebidder/src/ui/UserManagerBean.java
@@ -2,6 +2,7 @@ package ui;
 
 import db.UserDAO;
 import entities.User;
+
 import java.util.List;
 import java.util.Map;
 
@@ -12,237 +13,254 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
-
-
-
-@ManagedBean(name="manager")
+@ManagedBean(name = "manager")
 @SessionScoped
-//@ViewScoped
+// @ViewScoped
 public class UserManagerBean {
 
-    //private DataModel<UserBean> userList;
+	// private DataModel<UserBean> userList;
 
-    @ManagedProperty(value="#{userDAO}")
-    private UserDAO userDAO;
-    
-    
-    private List<User> userList;
-    
-    private String dot ="";
-    
+	@ManagedProperty(value = "#{userDAO}")
+	private UserDAO userDAO;
+
+	private List<User> userList;
+
+	private String dot = "";
+
 	private User user;
 
-    public void setUser(User user) {
-		this.user = user;
+	private boolean acceptButtonEnabled = false;
+
+	private boolean declineButtonEnabled = false;
+
+	private boolean banButtonEnabled = false;
+	
+	private int first;
+	
+	private int rows=3;
+	
+	public List<User> getUserlist() {
+		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String value = params.get("action");
+
+		if (!dot.equals(value) && value != null) // dot used to recognise what
+													// we need
+		{
+			if (value.equals("showall")) {
+				userList = userDAO.getUsers("all");
+				dot = "showall";
+				acceptButtonEnabled = false;
+				declineButtonEnabled = false;
+				banButtonEnabled = false;
+			} else if (value.equals("accepted")) {
+				userList = userDAO.getUsers("nopending");
+				dot = "accepted";
+				acceptButtonEnabled = false;
+				declineButtonEnabled = false;
+				banButtonEnabled = true;
+			} else {
+				userList = userDAO.getUsers("pending");
+				dot = "pending";
+				acceptButtonEnabled = true;
+				declineButtonEnabled = true;
+				banButtonEnabled = false;
+			}
+		}
+		return userList;
 	}
-    
-    boolean acceptButtonEnabled=false;
-    public boolean getAcceptButtonEnabled() {
-        return acceptButtonEnabled;
-    }
-    
 
-    boolean declineButtonEnabled=false;
-    public boolean getDeclineButtonEnabled() {
-        return declineButtonEnabled;
-    }
-    
-    boolean banButtonEnabled=false;
-    public boolean getBanButtonEnabled() {
-        return banButtonEnabled;
-    }
-    
-    
-	public List<User> getUserlist()
-    {
-    	Map<String,String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-    	String value = params.get("action");
-    	
-    	if( !dot.equals(value) && value!= null)  //dot used to recognise what we need
-        {
-        	//List<User> list;
-        	System.out.println("here");
-        	
-        	if(value.equals("showall")){
-        		userList = userDAO.getUsers("all"); 
-        		dot = "showall";
-        		acceptButtonEnabled = false;
-        		declineButtonEnabled = false;
-        		banButtonEnabled = false;
-        	}
-        	else if (value.equals("accepted")) {
-        		userList = userDAO.getUsers("nopending");
-        		dot = "accepted";
-        		acceptButtonEnabled = false;
-        		declineButtonEnabled = false;
-        		banButtonEnabled = true;
-        	}
-        	else {
-        		userList = userDAO.getUsers("pending");
-        		dot = "pending";
-        		acceptButtonEnabled = true;
-        		declineButtonEnabled = true;
-        		banButtonEnabled = false;
-        	}
-        }
-        System.out.println(value);
-        return userList;
-    }
-
-    public String edit(){
-    	System.out.println("EDIT---CURRENT");
-    	///////diaforetika koumpia gia kathena
-    	dot = "";
+	public String edit() {
+		System.out.println("EDIT---CURRENT");
+		/////// diaforetika koumpia gia kathena
+		dot = "";
 		return "/restricted/edit";
-    }
-    
-    
+	}
+
 	public UserDAO getUserDAO() {
 		return userDAO;
 	}
 
-
 	public void setUserDAO(UserDAO userDAO) {
 		this.userDAO = userDAO;
 	}
+
+	public String submit() {
+		System.out.println("SUBMIT---CURRENT");
+		System.out.println("EDIT---NEW");
+		dot = "";
+		userDAO.update(user);
+		return "/restricted/userlist";
+	}
+
+	public String accept() {
+		// dao update pending
+		user.setPending(0);
+		dot = "";
+		userDAO.update(user);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Successful Accept"));
+		return "/restricted/userlist"; // msg ok?
+	}
+
+	public String decline() {
+		// same
+		dot = "";
+		userDAO.remove(user);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Successful Operation"));
+		return "/restricted/userlist"; // msg ok?
+	}
 	
 	
-	public String submit(){
-    	System.out.println("SUBMIT---CURRENT");
-	    System.out.println("EDIT---NEW");
-	    dot = "";
-    	userDAO.update(user);
-    	return "/restricted/userlist";
-    }
-    
-    public String accept(){
-    	//dao update pending
-    	user.setPending(0);
-    	dot = "";
-    	userDAO.update(user);
-    	FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Successful Accept"));
-    	return "/restricted/userlist"; //msg ok?
-    }
-    
-    public String decline(){
-    	//same
-    	dot = "";
-    	userDAO.remove(user);
-    	FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Successful Operation"));
-    	return "/restricted/userlist"; //msg ok?
-    }
+	public String display(String action){
+		if(action.equals("next") ){
+			first += rows;
+		}
+		else{
+			first -= rows;
+		}
+		return null;
+	}
 
-    
-   public String goBack(){
-	   dot = "";
-	   return "/restricted/admin";
-   }
+	public boolean getNext(){
+		return first==userList.size() - userList.size()%rows;
+	}
 	
-	///////////////////////////////////////////////////
+	public boolean getPrev(){
+		return first==0;
+	}
 	
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-public int getUserID() {
-    return user.getUserID();
-  }
+	public boolean getAcceptButtonEnabled() {
+		return acceptButtonEnabled;
+	}
 
-  public void setUserID(int userID) {
-    user.setUserID(userID);
-  }
+	public boolean getDeclineButtonEnabled() {
+		return declineButtonEnabled;
+	}
 
-    
-    public String getCountry() {
-    return user.getCountry();
-  }
+	public boolean getBanButtonEnabled() {
+		return banButtonEnabled;
+	}
 
-  public void setCountry(String country) {
-    user.setCountry(country);
-  }
+	public String goBack() {
+		dot = "";
+		return "/restricted/admin";
+	}
+	
+	public int getFirst() {
+		return first;
+	}
 
-  public String getEmail() {
-    return user.getEmail();
-  }
+	public void setFirst(int first) {
+		this.first = first;
+	}
 
-  public void setEmail(String email) {
-    user.setEmail(email);
-  }
+	public int getRows() {
+		return rows;
+	}
 
-  public String getLocation() {
-    return user.getLocation();
-  }
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
 
-  public void setLocation(String location) {
-    user.setLocation(location);
-  }
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  public String getName() {
-    return user.getName();
-  }
+	public int getUserID() {
+		return user.getUserID();
+	}
 
-  public void setName(String name) {
-    user.setName(name);
-  }
+	public void setUserID(int userID) {
+		user.setUserID(userID);
+	}
 
-  public String getPassword() {
-    return user.getPassword();
-  }
+	public String getCountry() {
+		return user.getCountry();
+	}
 
-  public void setPassword(String password) {
-    user.setPassword(password);
-  }
+	public void setCountry(String country) {
+		user.setCountry(country);
+	}
 
-  public String getPhone() {
-    return user.getPhone();
-  }
+	public String getEmail() {
+		return user.getEmail();
+	}
 
-  public void setPhone(String phone) {
-    user.setPhone(phone);
-  }
+	public void setEmail(String email) {
+		user.setEmail(email);
+	}
 
-  public int getRating_Bidder() {
-    return user.getRating_Bidder();
-  }
+	public String getLocation() {
+		return user.getLocation();
+	}
 
-  public void setRating_Bidder(int rating_Bidder) {
-    user.setRating_Bidder(rating_Bidder);
-  }
+	public void setLocation(String location) {
+		user.setLocation(location);
+	}
 
-  public int getRating_Seller() {
-    return user.getRating_Seller();
-  }
+	public String getName() {
+		return user.getName();
+	}
 
-  public void setRating_Seller(int rating_Seller) {
-    user.setRating_Seller(rating_Seller);
-  }
+	public void setName(String name) {
+		user.setName(name);
+	}
 
-  public String getSurname() {
-    return user.getSurname();
-  }
+	public String getPassword() {
+		return user.getPassword();
+	}
 
-  public void setSurname(String surname) {
-    user.setSurname(surname);
-  }
+	public void setPassword(String password) {
+		user.setPassword(password);
+	}
 
-  public String getTaxRegistrationNumber() {
-    return user.getTaxRegistrationNumber();
-  }
+	public String getPhone() {
+		return user.getPhone();
+	}
 
-  public void setTaxRegistrationNumber(String taxRegistrationNumber) {
-    user.setTaxRegistrationNumber(taxRegistrationNumber);
-  }
+	public void setPhone(String phone) {
+		user.setPhone(phone);
+	}
 
-  public String getUsername() {
-    return user.getUsername();
-  }
+	public int getRating_Bidder() {
+		return user.getRating_Bidder();
+	}
 
-  public void setUsername(String username) {
-    user.setUsername(username);
-  }
-    
-    
-    
+	public void setRating_Bidder(int rating_Bidder) {
+		user.setRating_Bidder(rating_Bidder);
+	}
+
+	public int getRating_Seller() {
+		return user.getRating_Seller();
+	}
+
+	public void setRating_Seller(int rating_Seller) {
+		user.setRating_Seller(rating_Seller);
+	}
+
+	public String getSurname() {
+		return user.getSurname();
+	}
+
+	public void setSurname(String surname) {
+		user.setSurname(surname);
+	}
+
+	public String getTaxRegistrationNumber() {
+		return user.getTaxRegistrationNumber();
+	}
+
+	public void setTaxRegistrationNumber(String taxRegistrationNumber) {
+		user.setTaxRegistrationNumber(taxRegistrationNumber);
+	}
+
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	public void setUsername(String username) {
+		user.setUsername(username);
+	}
+
 }
-
-
-
-
-
