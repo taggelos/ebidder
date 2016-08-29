75fe07ab98c1e56e93de23af9023aa7fd52afc21
taggelos <aggelos5991@gmail.com>
2016-08-29 05:00:58 +0300

    trexoumena nera

---
+++ ebidder/src/jaxb/BidderXml.java
@@ -0,0 +1,85 @@
+package jaxb;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import entities.User;
+
+
+/**
+ * The persistent class for the item database table.
+ * 
+ */
+@XmlRootElement(name="Bidder")
+public class BidderXml implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String rating;
+	
+	private String userID;
+	
+	private String location;
+	
+	private String country;
+	
+	private User bidder = new User();
+
+	@XmlAttribute(name="Rating")
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	@XmlAttribute(name="UserID")
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	
+	@XmlJavaTypeAdapter(CollapsedStringAdapter.class)
+	@XmlElement(name="Location")
+	public String getLocation() {
+		return location;
+	}
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+	@XmlJavaTypeAdapter(CollapsedStringAdapter.class)
+	@XmlElement(name="Country")
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public void setBidder(){
+		bidder.setLocation(location);
+		//bidder.setUserID(Integer.parseInt(userID));
+		bidder.setCountry(country);
+		bidder.setRating_Bidder(Integer.parseInt(rating));
+	}
+	
+	public User getBidder(){
+		return bidder;
+	}
+	
+	
+}
---
+++ ebidder/src/jaxb/ItemsXml.java
@@ -0,0 +1,60 @@
+package jaxb;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the item database table.
+ * 
+ */
+@XmlRootElement(name="Items")
+public class ItemsXml implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private List<ItemXml> items;
+	
+	@XmlElement(name = "Item")
+	public List<ItemXml> getBids() {
+		if (null==items) {
+			items = new ArrayList<ItemXml>();
+		}
+		return items;
+	}
+
+	public void setPrograms(List<ItemXml> items) {
+		this.items = items;
+	}
+
+
+	public void setItems() {    
+		for (ItemXml i : items){
+			i.setItem();
+		}
+	}
+	
+	public List<ItemXml> getItems (){
+		return items;
+	}
+	/*
+	public List<Item> itemXmlToItem(List<ItemXml> itemxml){
+		List<Item> items = new ArrayList<>();
+		for (ItemXml i: itemxml)
+        {            
+			items.add(i.getItem());
+        }
+ 		return items;
+	}*/
+	
+	@Override
+	public String toString() {
+		return "Items [" + (items != null ? "items=" + items : "") + "]";
+	}
+}
